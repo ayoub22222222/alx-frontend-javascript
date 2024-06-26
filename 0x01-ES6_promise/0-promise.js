4ada50baf60032c9251d98bf5173382aa1eda020
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-06-26 17:42:28 +0000

    task 0 double 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
-	const nam = true;
-	return new Promise((resolve, reject) => {
-		if (nam) {
-			resolve();
-		} else {
-			reject();
-		}
-	});
+  const nam = true;
+  return new Promise((resolve, reject) => {
+    if (nam) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
