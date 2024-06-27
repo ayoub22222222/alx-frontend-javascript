df918fb05b18d21870bd7ee7435ea87cab4a1378
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-06-27 00:02:57 +0000

    task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-	const vm = {
-		status: 200,
-		body: 'success',
-	};
-	return promise
-		.then(() => ({vm}))
-	        .catch(() => new Error())
-	        .finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
