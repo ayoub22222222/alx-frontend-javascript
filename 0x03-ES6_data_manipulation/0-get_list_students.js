b907956f7a57a5895077a6fc5ff82691dfa2db63
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-07-02 17:00:59 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+default export function getListStudents() {
+    let var1 = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+        
+        ]
+    return var1;
+}
