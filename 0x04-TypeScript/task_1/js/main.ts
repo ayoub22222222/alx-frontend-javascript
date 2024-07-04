d5c2e3c862f5b75548dcb43051041931e00be213
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-07-04 01:13:22 +0000

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,5 +8,15 @@ interface Teacher {
 }
 
 interface Directors extends Teacher {
-	numberOfReports: string;
+	numberOfReports: number;
 }
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+
+function printTeacher(firstName, lastname): printTeacherFunction {
+	return `${firstanme[0]. ${lastname}}`;
+} 
+
