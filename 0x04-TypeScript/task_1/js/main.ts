0c0842f7ca78e4c05d030059e48cc9e83af4d11f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-07-04 01:23:02 +0000

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,5 +18,32 @@ interface printTeacherFunction {
 
 function printTeacher(firstName, lastname): printTeacherFunction {
 	return `${firstanme[0]. ${lastname}}`;
-} 
+}
 
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, StudentClass };
