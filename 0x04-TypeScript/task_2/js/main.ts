f504f400ad0867cb8e46021f1e190abdf938f394
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-07-04 01:51:11 +0000

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,4 +46,18 @@ function createEmployee(salary: string | number): Director | Teacher {
   	  }
 }
 
+function isDirector(employee: Teacher | Director): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director): string {
+  if (employee instanceof Director) {
+    return employee.workDirectorTasks();
+  } else if (employee instanceof Teacher) {
+    return employee.workTeacherTasks();
+  }
+}
+
+
+
 
